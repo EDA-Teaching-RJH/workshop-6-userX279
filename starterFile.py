f2194f0ed713f4f7c01e0cab293abccd4d919cbd
userX279 <sf581@kent.ac.uk>
2024-10-30 12:05:03 +0000

    rrfr

--- starterFile.py
+++ starterFile.py
@@ -3,11 +3,20 @@
 import math
 
 def safe_divide(a, b):
-
+    try:
+     return a/b 
+    except ZeroDivisionError:
+     print("cannot divide by 0")
 
 def process_list(input_list):
-
-
+ try: 
+     input_list
+     return ()
+ except TypeError:
+     print()
+ except ValueError:
+     print("wrong value inputed")
+      
 def nested_operations(a, b):
 
 
